c8d774d0d471f05a4f613c58c937589194fec578
gbergy <greg.goldberg718@gmail.com>
2014-04-20 15:32:18 -0400

    Fixed Twitter Auth

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140409202825) do
+ActiveRecord::Schema.define(version: 20140420191836) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 20140409202825) do
     t.datetime "updated_at"
     t.string   "provider"
     t.string   "uid"
+    t.string   "fname"
+    t.string   "lname"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
